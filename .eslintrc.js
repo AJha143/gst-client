d536dc9c739c0953c9a9a602ad2dd5e624b86a5f
Akshay <akshaykant5006@gmail.com>
2023-05-14 11:32:10 +0530

    added plugins in ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,37 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "plugin:react/recommended",
-        "airbnb"
+        "airbnb",
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended"
     ],
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+          }
     },
     "plugins": [
-        "react"
+        "react", "import", "jsx-a11y"
     ],
+    "settings": {
+        "react": {
+          "version": "detect"
+        }},
     "rules": {
+        "react/prop-types": 0,
+        "indent": ["error", 2],
+        "linebreak-style": 1,
+        "quotes": ["error", "double"]
     }
 }
