955ffc7ba64cf2ea709689522669c1247a6ef56e
Narendra Singh <singh.narendradeo@gmail.com>
2023-06-15 11:37:36 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import DrawerComp from "./Components/Drawer/DrawerComp";
 import TopMenuBar from "./Components/Navbar/TopMenuBar";
 import Routing from "./Routes/Routes";
-import { BrowserRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
@@ -10,37 +10,46 @@ import { useState, useEffect } from "react";
 function App() {
   const { loginDetails } = useSelector((state) => state.login);
   const [access_token, setAccessToken] = useState(
-    sessionStorage.getItem("accessToken")
+    sessionStorage.getItem("accessToken") || loginDetails.accessToken
   );
+  const [location, _setLocation] = useState(useLocation());
+  const [isNotFound, setIsNotFound] = useState(true);
+  const handleNotFound = () => {
+    setIsNotFound(false);
+  };
 
   useEffect(() => {
-    setAccessToken(
-      sessionStorage.getItem("accessToken") || loginDetails.accessToken
-    );
-    console.log("access_token", access_token);
-  }, [loginDetails.accessToken, access_token]);
+    setAccessToken(sessionStorage.getItem("accessToken"));
+  }, [access_token, sessionStorage.getItem("accessToken")]);
 
-  const Navigation = ({ token }) => {
-    console.log("token", token);
-    if (!token) return null;
-    return (
-      <Grid item xs={12}>
-        <TopMenuBar />
-        <DrawerComp />
-      </Grid>
-    );
+  const Navigation = () => {
+    if (access_token && isNotFound) {
+      return (
+        <Grid item xs={12}>
+          <TopMenuBar />
+          <DrawerComp />
+        </Grid>
+      );
+    }
+    if (access_token && !isNotFound) {
+      return;
+    }
   };
+
   return (
-    <BrowserRouter>
-      <Grid
-        container
-        classes={{ container: "appGridContainer" }}
-        direction="row"
-      >
-        <Navigation token={access_token} />
-        <Routing />
-      </Grid>
-    </BrowserRouter>
+    // <BrowserRouter>
+    <Grid
+      item
+      container
+      classes={{ container: "appGridContainer" }}
+      direction="row"
+    >
+      {/* <Routing /> */}
+      {Navigation()}
+      <Routing handleNotFound={handleNotFound} />
+      
+    </Grid>
+    // </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import AuthenticatedComponent from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
       <Provider store={store}>
         <AuthenticatedComponent />
       </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
